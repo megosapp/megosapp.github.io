c3693d2f370615237bec10b7b0a73330550f77b0
megos <gmegos.27@gmail.com>
2018-10-17 08:56:09 +0900

    Add header image

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <div class="text-xs-center">
-        <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      </div>
+      <v-card>
+        <v-card-media>
+        <v-img src="/header.jpg" alt="header" />
+        </v-card-media>
+      </v-card>
       <v-card>
         <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
         <v-card-text>
