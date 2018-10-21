31397dfd2bce994eac4e661e89a7cd1c559ac7c7
megos <gmegos.27@gmail.com>
2018-10-21 16:49:39 +0900

    Fix title

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
         </v-responsive>
       </v-card>
       <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
+        <v-card-title class="headline">桜島の降灰予報をあなたの手のひらで</v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
           <p>For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank">documentation</a>.</p>
