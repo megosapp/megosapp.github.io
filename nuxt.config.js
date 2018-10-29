d56257b0cf5d9f29790e6e185224f1cc5c8507cf
megos <gmegos.27@gmail.com>
2018-10-29 21:51:52 +0900

    Add og prefix

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,10 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
-    ]
+    ],
+    htmlAttrs: {
+      prefix: 'og: http://ogp.me/ns#'
+    }
   },
   plugins: ['~/plugins/vuetify.js'],
   modules: [
