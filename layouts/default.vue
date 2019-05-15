76a6e4260e7c79e79eb3d0ccfc8765738d495824
megos <gmegos.27@gmail.com>
2019-05-15 11:07:38 +0900

    eslint —fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,36 @@
 <template>
   <v-app>
-    <v-toolbar fixed dark app color="primary" :clipped-left="clipped">
-      <v-toolbar-title v-text="title"/>
+    <v-toolbar
+      fixed
+      dark
+      app
+      color="primary"
+      :clipped-left="clipped"
+    >
+      <v-toolbar-title v-text="title" />
     </v-toolbar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer :fixed="fixed" app>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
       <span>&copy; MegosApp</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        clipped: false,
-        fixed: false,
-        title: '桜島降灰予報'
-      }
+export default {
+  data() {
+    return {
+      clipped: false,
+      fixed: false,
+      title: '桜島降灰予報',
     }
-  }
+  },
+}
 </script>
