8aa700265b45a4d4b65b6f12dc9c9a6bf7a313ed
megos <gmegos.27@gmail.com>
2019-09-11 00:38:48 +0900

    Re yarn create nuxt-app

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
   /*
   ** Headers of the page
@@ -22,36 +24,62 @@ export default {
       prefix: 'og: http://ogp.me/ns#',
     },
   },
-  plugins: ['~/plugins/vuetify.js'],
   modules: [
     ['@nuxtjs/google-analytics', {
       id: 'UA-37532600-3',
     }],
-  ],
-  css: [
-    '~/assets/style/app.styl',
   ],
   /*
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
   /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify'
+  ],
+  /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+  /*
   ** Build configuration
   */
   build: {
-    extractCSS: true,
     /*
-    ** Run ESLint on save
+    ** You can extend webpack config here
     */
-    extend(config, { isDev }) {
-      if (isDev && process.client) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/,
-        })
-      }
-    },
-  },
+    extend (config, ctx) {
+    }
+  }
 }
