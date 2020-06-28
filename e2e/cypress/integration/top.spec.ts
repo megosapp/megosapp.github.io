577592d1ab008dec124b7de321dea32d05874d5a
megos <gmegos.27@gmail.com>
2020-06-28 11:00:57 +0900

    Introduce applitools

--- e2e/cypress/integration/top.spec.ts
+++ e2e/cypress/integration/top.spec.ts
@@ -3,4 +3,25 @@ describe('Top', () => {
     cy.visit('/')
     cy.contains('桜島降灰予報')
   })
+
+  it('cross browser', () => {
+    cy.visit('/')
+
+    // Call Open on eyes to initialize a test session
+    cy.eyesOpen({
+      appName: 'Sakurajima wind',
+      testName: 'cross browser'
+    })
+
+    // check the login page with fluent api, see more info here
+    // https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html
+    cy.eyesCheckWindow({
+      tag: 'Top Window',
+      target: 'window',
+      fully: true
+    })
+
+    // Call Close on eyes to let the server know it should display the results
+    cy.eyesClose()
+  })
 })
